e8b600b2c53ffbd05f69a1d7ee002d5c8cfb9ec3
Ben Judd <ben.judd@10gen.com>
2018-07-16 23:20:33 -0400

    Added authorization check

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 import { PLATFORM } from 'aurelia-pal';
+import { inject } from 'aurelia-framework';
+import {Redirect} from 'aurelia-router';
+import { StitchResources } from './stitch_resources';
 
 export class App {
   configureRouter(config, router) {
     config.title = 'Hackathon';
     config.options.pushState = true;
+    config.addAuthorizeStep(AuthorizeStep);
     config.map([
       { route: ['index', 'home', ''], moduleId: PLATFORM.moduleName('homepage'), title: 'Home' },
-      { route: 'me', moduleId: PLATFORM.moduleName('user-detail'), title: 'Me'},
-      { route: 'confirm/', moduleId: PLATFORM.moduleName('confirm-email'), title: 'Confirm'}
+      { route: 'me', moduleId: PLATFORM.moduleName('user-detail'), title: 'Me', settings:{auth:true} },
+      { route: 'confirm/', moduleId: PLATFORM.moduleName('confirm-email'), title: 'Confirm' }
     ]);
     this.router = router;
+  }
+}
+@inject(StitchResources)
+class AuthorizeStep {
+  constructor(stitchResources) {
+    this.stitch = stitchResources;
+  }
+  run(navigationInstruction, next) {
+    if (navigationInstruction.getAllInstructions().some(i => i.config.settings.auth)) {
+      if (! this.stitch.client().auth.isLoggedIn) {
+        console.log('Not Authenticated');
+        return next.cancel(new Redirect('home'));
+      }
+    }
+    return next();
   }
 }
